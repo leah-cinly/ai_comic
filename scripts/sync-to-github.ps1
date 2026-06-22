61e9ad534ce0013955c47b6dab709968b8a50acd
leah-cinly <2982700365@qq.com>
2026-06-23 02:42:55 +0800

    Make GitHub sync setup message friendly

--- scripts/sync-to-github.ps1
+++ scripts/sync-to-github.ps1
@@ -11,8 +11,8 @@ if (-not (Test-Path ".git")) {
   git branch -M main
 }
 
-$remote = git remote get-url origin 2>$null
-if (-not $remote) {
+$remote = & git remote get-url origin 2>$null
+if ($LASTEXITCODE -ne 0 -or -not $remote) {
   Write-Host "还没有设置 GitHub 远程仓库。请先运行："
   Write-Host "git remote add origin https://github.com/你的用户名/你的仓库名.git"
   exit 1
